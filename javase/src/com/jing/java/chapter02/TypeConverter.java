8ed6d14e8648c02de38cc4e945de45f8d9d76d50
FiddlerBlue <37952043+FiddlerBlue@users.noreply.github.com>
2019-10-02 06:09:48 +0800

    add one line of printing to console.

--- javase/src/com/jing/java/chapter02/TypeConverter.java
+++ javase/src/com/jing/java/chapter02/TypeConverter.java
@@ -3,5 +3,6 @@ package com.jing.java.chapter02;
 public class TypeConverter {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        System.out.println("Just want to check how conflict works!";
     }
 }
