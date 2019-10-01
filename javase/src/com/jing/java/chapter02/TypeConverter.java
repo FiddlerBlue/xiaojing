61d412db000f403661eb89075b16eff9360dc164
Z003PXAS <xiaojing.zhao@siemens.com>
2019-10-02 06:10:54 +0800

    test strong converting

--- javase/src/com/jing/java/chapter02/TypeConverter.java
+++ javase/src/com/jing/java/chapter02/TypeConverter.java
@@ -3,5 +3,9 @@ package com.jing.java.chapter02;
 public class TypeConverter {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        byte b = 10;
+        int a = 20;
+        byte c = (byte) (a + b);
+        System.out.println(c);
     }
 }
