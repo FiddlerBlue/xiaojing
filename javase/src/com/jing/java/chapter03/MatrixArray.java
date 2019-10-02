5b6821e1f5a71eddb5f5bc2dda316f17e6ad8e4f
Z003PXAS <xiaojing.zhao@siemens.com>
2019-10-02 09:51:56 +0800

    Create MatrixArray.java

---
+++ javase/src/com/jing/java/chapter03/MatrixArray.java
@@ -0,0 +1,25 @@
+package com.jing.java.chapter03;
+
+public class MatrixArray {
+    public static void main(String[] args) {
+        int[][] array = new int[3][];
+        array[0] = new int[5];
+        array[1] = new int[3];
+        array[2] = new int[4];
+        array[0][0] = 1;
+        array[0][1] = 2;
+        array[0][2] = 3;
+        array[0][3] = 4;
+        array[0][4] = 5;
+        //System.out.println(array[1][1]);
+
+        int[][] arrayInit = {{1,2},{3,4,5},{6,7,8,9}};
+        //System.out.println(arrayInit[2][3]);
+        int[] arraySource = {1,2,3,4,5,6,7,8,9};
+        int[] arrayDest = new int[10];
+        System.arraycopy(arraySource,2,arrayDest,4,3);
+        for (int i = 0; i < arrayDest.length; i++){
+            System.out.println(arrayDest[i]);
+        }
+    }
+}
